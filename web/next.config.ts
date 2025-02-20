9c7aa011fa4c62c2bea085eb68d7f0769b5b2fc1
Michael lemito <delovoy.lemit@gmail.com>
2025-02-20 22:44:46 +0300

    мяу

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  swcMinify: true,
+  output: "export",
+  experimental: {
+    forceSwcTransforms: true, 
+  },
+  webpack: (config: any) => {
+    config.optimization = {
+      ...config.optimization,
+      usedExports: true,
+      minimize: true, 
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
