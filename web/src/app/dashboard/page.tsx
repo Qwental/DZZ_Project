a5dd78754c32110a55c7183cbda3387b8c2e43ca
Michael lemito <delovoy.lemit@gmail.com>
2025-03-03 23:58:13 +0300

    макет раскидал

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -2,9 +2,9 @@ import styles from "./page.module.css";
 
 export default function DahboardPage() {
     return (
-        <div className={styles.page}>
-            <div></div>
-            <div></div>
-        </div>
-    )
+      <div className={styles.page}>
+        <div className={styles.resultsZone}></div>
+        <div className={styles.profileZone}></div>
+      </div>
+    );
 }
