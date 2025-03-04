1434ada3b6e53de41b72e240aae5827a8c9325bd
emppress <rozhkovvanya.2005@yandex.ru>
2025-03-04 21:46:19 +0300

    new user views

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,22 +1,18 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
 
-from users.views import UserViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include('users.urls'), name='users'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,12 +1,39 @@
-from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import permissions
+from .serializers import UserSerializer, RegisterSerializer
+from rest_framework import generics
 from .serializers import UserSerializer
-from .permissions import UserPermission
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from rest_framework.response import Response
 
 
-class UserViewSet(ModelViewSet):
+class UserDetailView(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
     serializer_class = UserSerializer
-    queryset = User.objects.all().order_by("-date_joined")
-    permission_classes = [UserPermission,]
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
+
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class LogoutView(generics.GenericAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, format=None):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
