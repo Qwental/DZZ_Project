bb095849e01c427a1e41e1239aedd22fd1e461d9
emppress <rozhkovvanya.2005@yandex.ru>
2025-03-09 21:49:27 +0300

    update upload_api

--- myproject/api_app/serializers.py
+++ myproject/api_app/serializers.py
@@ -1,26 +1,9 @@
 from rest_framework import serializers
 from .models import ProcessedImage
 
+
 class ProcessedImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProcessedImage
         fields = ['id', 'image', 'created_at']
         read_only_fields = ['id', 'created_at']
-
-
-class UploadMultipleImagesSerializer(serializers.Serializer):
-    images = serializers.ListField(
-        child=serializers.ImageField(),
-        allow_empty=False
-    )
-
-    def create(self, validated_data):
-        user = self.context["request"].user
-        images = validated_data.get("images")
-        processed_images = []
-
-        for image in images:
-            processed_image = ProcessedImage.objects.create(image=image, user=user)
-            processed_images.append(processed_image)
-
-        return processed_images
--- myproject/api_app/views.py
+++ myproject/api_app/views.py
@@ -3,49 +3,47 @@ import shutil
 from django.conf import settings
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from .models import ProcessedImage
-from .serializers import UploadMultipleImagesSerializer, ProcessedImageSerializer
+from .serializers import ProcessedImageSerializer
 from .yolo_model import predict
 
 
-class UploadImageView(CreateAPIView):
-    serializer_class = UploadMultipleImagesSerializer
+class UploadImageView(APIView):
     permission_classes = [IsAuthenticated]
-    parser_classes = (MultiPartParser, FormParser)  # Обработчики для multipart/form-data
+    # Обработчики для multipart/form-data
+    parser_classes = (MultiPartParser, FormParser)
 
-    def create(self, request, *args, **kwargs):
+    def post(self, request: Request, *args, **kwargs):
         user = request.user
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        images = serializer.validated_data['images']
         uploaded_images = []
+
+        if not request.FILES:
+            return Response({"error": "No files were uploaded."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
         # Папка для медиа (для сохранения оригинальных изображений)
         media_dir = os.path.join(settings.MEDIA_ROOT, 'images')
         os.makedirs(media_dir, exist_ok=True)
 
         # Папка для обработанных изображений
-        processed_dir = settings.PROCESSED_IMAGES_ROOT
+        processed_dir = os.path.join(
+            settings.MEDIA_ROOT, settings.PROCESSED_IMAGES_ROOT)
         os.makedirs(processed_dir, exist_ok=True)
 
-        for image in images:
-            # Сохраняем изображение в папку media/images
-            image_path = os.path.join(media_dir, image.name)
+        for image_field_name in request.FILES:
+            image_file = request.FILES[image_field_name]
+
+            image_path = os.path.join(media_dir, image_file.name)
             with open(image_path, 'wb') as f:
-                for chunk in image.chunks():
+                for chunk in image_file.chunks():
                     f.write(chunk)
 
             # Запускаем нейросеть для обработки изображения
-            processed_image_path, _ = predict(image_path)
-
-            # Перемещаем результат в папку processed_images
-            processed_image_name = os.path.basename(processed_image_path)
-            processed_image_path_new = os.path.join(processed_dir, processed_image_name)
-            shutil.move(processed_image_path, processed_image_path_new)
+            relative_path, _ = predict(image_path, processed_dir)
 
             # Удаляем временное изображение из папки media
             if os.path.exists(image_path):
@@ -54,12 +52,13 @@ class UploadImageView(CreateAPIView):
             # Сохраняем обработанное изображение в базе данных
             uploaded_image = ProcessedImage.objects.create(
                 user=user,
-                image=processed_image_path_new,  # Путь к результату в папке processed_images
+                image=relative_path,
             )
             uploaded_images.append(uploaded_image)
 
         # Сериализуем обработанные изображения
-        serialized_images = ProcessedImageSerializer(uploaded_images, many=True)
+        serialized_images = ProcessedImageSerializer(
+            uploaded_images, many=True)
 
         return Response({
             "message": "Images processed",
